29dc4f9c3f1f71b45002299e20cac331c73f4c9a
Raphael Barreto <rfsbarreto@gmail.com>
2024-02-05 14:20:38 -0300

    Adds presence_validations to Transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,10 @@
 class Transaction < ApplicationRecord
   has_one :user
   has_one :merchant
+
+  validates_presence_of :merchant_id, :user_id, :card_number, :transaction_date, :transaction_amount
+
+  def recommendation
+    approved? ? 'approved' : 'denied'
+  end
 end
