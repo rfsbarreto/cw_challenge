0252717ae5ecd2ec742c1f439cc25c30003ef678
Raphael Barreto <rfsbarreto@gmail.com>
2024-02-06 22:56:18 -0300

    Improve transaction, merchant and user specs

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -10,48 +10,83 @@ RSpec.describe Transaction, type: :model do
   end
 
   describe 'associations' do
-    it {  }
+    it { should belong_to(:user) }
+    it { should belong_to(:merchant) }
   end
 
-  describe '#by_user' do
-    let(:user) { create(:user) }
-    let!(:transaction) { create(:transaction, user_id: user.id) }
-    let!(:other_transaction) { create(:transaction) }
+  describe 'scopes' do
+    describe '.by_user' do
+      let(:user) { create(:user) }
+      let!(:transaction) { create(:transaction, user_id: user.id) }
+      let!(:other_transaction) { create(:transaction) }
 
-    context 'when user exist' do
-      it 'returns only transactions by the passed user' do
-        expect(Transaction.by_user(user.id)).to match [transaction]
+      context 'when user exist' do
+        it 'returns only transactions by the passed user' do
+          expect(Transaction.by_user(user.id)).to match [transaction]
+        end
+      end
+
+      context 'when user doesnt exist' do
+        it { expect(Transaction.by_user(-1)).to be_empty }
       end
     end
 
-    context 'when user doesnt exist' do
-      it { expect(Transaction.by_user(-1)).to be_empty }
-    end
-  end
+    describe '.number_of_transactions_by_timeframe' do
+      subject(:number_of_transactions_by_timeframe) do
+        Transaction.number_of_transactions_by_timeframe(time_in_seconds: 66.minutes)
+      end
+      let!(:transaction1) { create(:transaction, transaction_date: 1.hour.ago) }
+      let!(:transaction2) { create(:transaction, transaction_date: 2.hours.ago) }
+      let!(:transaction3) { create(:transaction, transaction_date: 3.hours.ago) }
 
-  describe '.number_of_transactions_by_timeframe' do
-    subject(:number_of_transactions_by_timeframe) do
-      Transaction.number_of_transactions_by_timeframe(time_in_seconds: 66.minutes)
+      it 'returns 1' do
+        expect(number_of_transactions_by_timeframe).to eq 1
+      end
     end
-    let!(:transaction1) { create(:transaction, transaction_date: 1.hour.ago) }
-    let!(:transaction2) { create(:transaction, transaction_date: 2.hours.ago) }
-    let!(:transaction3) { create(:transaction, transaction_date: 3.hours.ago) }
 
-    it 'returns 1' do
-      expect(number_of_transactions_by_timeframe).to eq 1
+    describe '.with_chargeback' do
+      let!(:transaction2) { create(:transaction, chargebacked: false) }
+
+      context 'when there are transactions with chargeback' do
+        let!(:transaction1) { create(:transaction, chargebacked: true) }
+
+        it 'returns transactions with chargeback' do
+          expect(Transaction.with_chargeback).to eq([transaction1])
+        end
+      end
+
+      context 'when no transactions were chargebacked' do
+        it 'returns transactions with chargeback' do
+          expect(Transaction.with_chargeback).to be_empty
+        end
+      end
+    end
+
+    describe '.weekly' do
+      let!(:transaction1) { create(:transaction, transaction_date: Time.now - 6.days) }
+      let!(:transaction2) { create(:transaction, transaction_date: Time.now - 8.days) }
+
+      it 'returns transactions within the past week' do
+        expect(Transaction.weekly).to eq([transaction1])
+      end
     end
   end
 
   describe '#recommendation' do
-    let(:approved_transaction) { create(:transaction, approved: true) }
-    let(:denied_transaction) { create(:transaction, approved: false) }
+    let(:transaction) { create(:transaction, approved:) }
 
-    it 'returns "approved" for approved transactions' do
-      expect(approved_transaction.recommendation).to eq('approved')
+    context 'when approved' do
+      let(:approved) { true }
+      it 'returns "approved" for approved transactions' do
+        expect(transaction.recommendation).to eq('approved')
+      end
     end
 
-    it 'returns "denied" for denied transactions' do
-      expect(denied_transaction.recommendation).to eq('denied')
+    context 'when approved is false' do
+      let(:approved) { false }
+      it 'returns "denied" for denied transactions' do
+        expect(transaction.recommendation).to eq('denied')
+      end
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:transactions) }
+  end
+
+  describe '.transaction_amount_limit' do
+    subject(:transaction_amount_limit) { user.transaction_amount_limit }
+    let(:user) { create(:user, max_transaction_amount: max_amount) }
+
+    context 'when user has max_transaction_amount set up' do
+      let(:max_amount) { 10_000 }
+
+      it { expect(transaction_amount_limit).to eq max_amount }
+    end
+
+    context 'when user has max_transaction_amount set up' do
+      let(:max_amount) { nil }
+
+      it { expect(transaction_amount_limit).to eq ENV['DEFAULT_MAX_TRANSACTION_AMOUNT'].to_f }
+    end
+  end
 end
